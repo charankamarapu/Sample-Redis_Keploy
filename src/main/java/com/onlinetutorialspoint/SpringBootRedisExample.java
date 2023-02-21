887875e688c674831bbbb4d4299dc6c436500c7e
charankamarapu <kamarapucharan@gmail.com>
2023-02-21 13:25:52 +0530

    Adding Keploy changes

--- src/main/java/com/onlinetutorialspoint/SpringBootRedisExample.java
+++ src/main/java/com/onlinetutorialspoint/SpringBootRedisExample.java
@@ -1,13 +1,16 @@
 package com.onlinetutorialspoint;
 
 import com.onlinetutorialspoint.model.Item;
+import io.keploy.servlet.KeployMiddleware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
+@Import(KeployMiddleware.class)
 public class SpringBootRedisExample {
 
 	@Bean
